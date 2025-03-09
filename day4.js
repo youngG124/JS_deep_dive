a0ab9aa8d4f885c534371a5c3cc587b26ab3b058
newyoung124 <newyoung124@naver.com>
2025-03-09 21:51:37 +0900

    scope

--- day4.js
+++ day4.js
@@ -1,3 +1,5 @@
+// 2025.3.9 bojung, KK
+
 function changeVal(primitive, obj) {
     primitive += 100;
     obj.name = 'Kim';
@@ -10,3 +12,16 @@ changeVal(num, person);
 
 console.log(num); // primitive value doesn't changed
 console.log(person); // but object, the value changed
+
+/////////// scope ////
+
+var x = 'global';
+
+function foo() {
+    var x = 'local';
+    console.log(x);
+}
+
+foo();
+
+console.log(x);

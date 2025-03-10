8f8cadad1fe43db18848fb80ad4ee86043d6dd59
newyoung124 <newyoung124@naver.com>
2025-03-10 23:34:52 +0900

    day5

---
+++ day5.js
@@ -0,0 +1,10 @@
+/**
+ * day 5
+ * after work, home, alone. Javscript Deep Dive page 200, problem of global variable 
+ * */
+
+const str = String(123);
+const str2 = new String(123);
+
+console.log(str, typeof str);
+console.log(str2, typeof str2);

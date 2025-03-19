7943b470654f1573142125b843f6cd2ed889a42d
newyoung124 <newyoung124@naver.com>
2025-03-19 19:55:36 +0900

    day 9

---
+++ day9.js
@@ -0,0 +1,12 @@
+const x = 1;
+
+function outer() {
+    const x = 10;
+    const inner = function () {
+        console.log(x);
+    }
+    return inner;
+}
+
+const innerFunc = outer();
+innerFunc();
